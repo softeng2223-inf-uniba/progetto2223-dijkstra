9858581e721e00f18d5572067107f0ebe97d50e1
NicoResta <n.resta6@studenti.uniba.it>
2023-06-03 17:30:19 +0200

    revert "adattata la classe Difficulty ai nuovi requisiti funzionali"
    This reverts commit 7bd99bcadff57dccf9cc8d09a7fd8cf3ed7ff827.

--- src/main/java/it/uniba/app/Difficulty.java
+++ src/main/java/it/uniba/app/Difficulty.java
@@ -1,85 +1,23 @@
 package it.uniba.app;
-
 /**
- * Class that represents the game difficulty.
+ * Enum class for the match difficulty.
  */
-public final class Difficulty {
-    public static final int NUMBER_OF_DIFFICULTIES = 3;
+public enum Difficulty {
+    EASY(50),
+    MEDIUM(30),
+    HARD(10);
 
-    public static final int DEFAULT_EASY_MAX_FAILURES = 50;
-    public static final int DEFAULT_MEDIUM_MAX_FAILURES = 30;
-    public static final int DEFAULT_HARD_MAX_FAILURES = 10;
-
-    private static int[] difficulties = {DEFAULT_EASY_MAX_FAILURES,
-        DEFAULT_MEDIUM_MAX_FAILURES, DEFAULT_HARD_MAX_FAILURES};
-
+    private final int maxFailures;
     /**
-     * Enumeration that represents all difficulties levels implemented.
+     * Difficulty constructor.
+     * @param inMaxFailures - specifies the maximum number of failures.
      */
-    public enum Level {
-        EASY, MEDIUM, HARD
+    Difficulty(final int inMaxFailures) {
+        this.maxFailures = inMaxFailures;
     }
 
-    private Level currentLevel;
-
-    /**
-     * Constructor for Difficulty class.
-     * @param level - sets the current difficulty level.
-     */
-    public Difficulty(final Level level) {
-        currentLevel = level;
+    public int getMaxFailures() {
+        return maxFailures;
     }
 
-    public void setCurrentLevel(final Level level) {
-        currentLevel = level;
-    }
-
-    public Level getCurrentLevel() {
-        return currentLevel;
-    }
-
-    public int getCurrentMaxFailures() {
-        return difficulties[currentLevel.ordinal()];
-    }
-
-    public static int getEasyMaxFailures() {
-        return difficulties[Level.EASY.ordinal()];
-    }
-
-    public static void setEasyMaxFailures(final int easyMaxFailures) {
-        difficulties[Level.EASY.ordinal()] = easyMaxFailures;
-    }
-
-    public static int getMediumMaxFailures() {
-        return difficulties[Level.MEDIUM.ordinal()];
-    }
-
-    public static void setMediumMaxFailures(final int mediumMaxFailures) {
-        difficulties[Level.MEDIUM.ordinal()] = mediumMaxFailures;
-    }
-
-    public static int getHardMaxFailures() {
-        return difficulties[Level.HARD.ordinal()];
-    }
-
-    public static void setHardMaxFailures(final int hardMaxFailures) {
-        difficulties[Level.HARD.ordinal()] = hardMaxFailures;
-    }
-
-    /**
-     * .
-     * @return
-     */
-    public String getName() {
-        switch (currentLevel) {
-            case EASY:
-                return "Facile";
-            case MEDIUM:
-                return "Medio";
-            case HARD:
-                return "Difficile";
-            default:
-                return null;
-        }
-    }
 }
