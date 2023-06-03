fafbd73b83cfd32b754120c4d178ab9cd1ad8392
NicoResta <n.resta6@studenti.uniba.it>
2023-06-03 11:00:37 +0200

    aggiornata la gestione della dimensione della mappa

--- src/main/java/it/uniba/app/Map.java
+++ src/main/java/it/uniba/app/Map.java
@@ -6,56 +6,60 @@ import java.util.Random;
  * This class describe the behaviour of the map during the game.
  */
 public final class Map {
-    public static final int CELLS_NUMBER = 10;
-    public static final int DESTROYERS_NUMBER = 4;
-    public static final int CRUISERS_NUMBER = 3;
-    public static final int ARMOUREDS_NUMBER = 2;
-    public static final int AIRCRAFT_CARRIERS_NUMBER = 1;
+    private MapType type;
     private Cell[][] map;
 
     /**
      * Map Constructor that initializes the grid with random ship positioning.
      */
-    public Map() {
+    public Map(final MapType inType) {
         Random rand = new Random();
         int row;
         int col;
 
-        map = new Cell[CELLS_NUMBER][CELLS_NUMBER];
+        final int destroyersNumber = ShipType.DESTROYER.getSize();
+        final int cruisersNumber = ShipType.DESTROYER.getSize();
+        final int armouredsNumber = ShipType.ARMOURED.getSize();
+        final int aircraftCarriersNumber = ShipType.AIRCRAFT_CARRIER.getSize();
 
-        for (int i = 0; i < CELLS_NUMBER; i++) {
-            for (int j = 0; j < CELLS_NUMBER; j++) {
+        type = inType;
+        int size = type.getSize();
+
+        map = new Cell[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 if (map[i][j] == null) {
                     map[i][j] = new Cell();
                 }
             }
         }
 
-        for (int i = 0; i < DESTROYERS_NUMBER; i++) {
+        for (int i = 0; i < destroyersNumber; i++) {
             do {
-                row = rand.nextInt(CELLS_NUMBER);
-                col = rand.nextInt(CELLS_NUMBER);
+                row = rand.nextInt(size);
+                col = rand.nextInt(size);
             } while (!setDirection(row, col, new Ship(ShipType.DESTROYER)));
         }
 
-        for (int i = 0; i < CRUISERS_NUMBER; i++) {
+        for (int i = 0; i < cruisersNumber; i++) {
             do {
-                row = rand.nextInt(CELLS_NUMBER);
-                col = rand.nextInt(CELLS_NUMBER);
+                row = rand.nextInt(size);
+                col = rand.nextInt(size);
             } while (!setDirection(row, col, new Ship(ShipType.CRUISER)));
         }
 
-        for (int i = 0; i < ARMOUREDS_NUMBER; i++) {
+        for (int i = 0; i < armouredsNumber; i++) {
             do {
-                row = rand.nextInt(CELLS_NUMBER);
-                col = rand.nextInt(CELLS_NUMBER);
+                row = rand.nextInt(size);
+                col = rand.nextInt(size);
             } while (!setDirection(row, col, new Ship(ShipType.ARMOURED)));
         }
 
-        for (int i = 0; i < AIRCRAFT_CARRIERS_NUMBER; i++) {
+        for (int i = 0; i < aircraftCarriersNumber; i++) {
             do {
-                row = rand.nextInt(CELLS_NUMBER);
-                col = rand.nextInt(CELLS_NUMBER);
+                row = rand.nextInt(size);
+                col = rand.nextInt(size);
             } while
             (!setDirection(row, col, new Ship(ShipType.AIRCRAFT_CARRIER)));
         }
@@ -64,7 +68,7 @@ public final class Map {
     private boolean checkRight(final int row, final int col, final int size) {
         int i = 0;
 
-        if (col > CELLS_NUMBER - size) {
+        if (col > size - size) {
             return false;
         }
         while (i < size && map[row][col + i].isFree()) {
@@ -77,7 +81,7 @@ public final class Map {
     private boolean checkBelow(final int row, final int col, final int size) {
         int i = 0;
 
-        if (row > CELLS_NUMBER - size) {
+        if (row > size - size) {
             return false;
         }
         while (i < size && map[row + i][col].isFree()) {
@@ -142,6 +146,10 @@ public final class Map {
         return set;
     }
 
+    /**
+     * Returns the revelead grid with all ship positions.
+     * @return String - represents the grid in a formatted String.
+     */
     @Override
     public String toString() {
         StringBuffer s = new StringBuffer();
@@ -207,7 +215,7 @@ public final class Map {
      * Returns a string that describes all alive and sunken ships.
      * @return String
      */
-    public String shipStats() {
+    public String getShipStats() {
         StringBuffer s = new StringBuffer();
         int carriers = 0;
         int armoureds = 0;
