bfe8faf24448eca2932216f5c24e5597602609f9
Daniele Tonti <d.tonti1@studenti.uniba.it>
2023-06-10 19:19:34 +0200

    implementata classe RevealGridTest.java

---
+++ src/test/java/it/uniba/app/battleship/RevealGridTest.java
@@ -0,0 +1,41 @@
+package it.uniba.app.battleship;
+
+import static it.uniba.app.App.revealGrid;
+import static it.uniba.app.utils.OutputHandler.captureOutput;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/*
+ * RevealGridTest - Test class used to check
+ * the behaviour of the "revealGrid()" method
+ * of the class "App" under certain conditions
+*/
+class RevealGridTest {
+
+    /**
+     * Tests the method {@link App#revealGrid()} to check
+     * if the app prevents the user from seeing the grid,
+     * if the match has not started yet.
+     */
+    @Test
+    @DisplayName("Check if the grid hasn't been revealed, if the match hasn't started yet.")
+    void testRevealGridMatchNull() {
+        final int cr = 13;
+        String expectedOutput = "La partita non e' ancora iniziata.\nDigita /gioca per iniziare una nuova partita!";
+
+        /*
+         * The "replace" method is used to remove control characters
+         * which may prevent the result string from being compared
+         * correctly with the expected output.
+         */
+        String output = captureOutput(() -> revealGrid())
+                        .replace((char) (cr) + "\n", "\n");
+
+        assertTrue(expectedOutput.toString().compareTo(output) == 0,
+            "The grid should not be revealed,"
+            + "because the match hasn't started yet.");
+    }
+}
